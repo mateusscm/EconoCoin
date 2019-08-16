d8504222e6b229c6cec7a393b58ae965f4ed0671
mateusscm <mateus.miranda@hotmail.com.br>
2019-08-15 21:02:47 -0300

    page's navigation part 2

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,53 @@
-// import React from "react";
+import React from "react";
 import {} from "react-native";
 import {
   createSwitchNavigator,
   createAppContainer,
-  createStackNavigator
+  createStackNavigator,
+  createDrawerNavigator
 } from "react-navigation";
+import Icon from "react-native-vector-icons/Ionicons";
 
 // Importing Views
 import Login from "../views/Login/Login";
 import SignUp from "../views/SignUp/SignUp";
 import Forget from "../views/Forget/Forget";
-import AuthLoading from "../views/AuthLoading/AuthLoading";
+// import AuthLoading from "../views/AuthLoading/AuthLoading";
 import Home from "../views/Home/Home";
 
-const LoginStackNavigator = createStackNavigator({
+const HomeStackNavigator = createStackNavigator(
+  {
+    Home: {
+      screen: Home,
+      navigationOptions: ({ navigation }) => ({
+        title: "Home"
+      })
+    }
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => {
+      return {
+        headerLeft: (
+          <Icon
+            style={{ paddingLeft: 10 }}
+            onPress={() => navigation.openDrawer()}
+            name="md-menu"
+            size={30}
+          />
+        )
+      };
+    }
+  }
+);
+
+const AppDrawerNavigator = createDrawerNavigator({
+  Home: {
+    screen: HomeStackNavigator
+  }
+});
+
+const Navigator = createSwitchNavigator({
+  // AuthLoading: AuthLoading,
   Login: {
     screen: Login,
     navigationOptions: {
@@ -22,12 +56,9 @@ const LoginStackNavigator = createStackNavigator({
   },
   SignUp: SignUp,
   Forget: Forget,
-  Home: Home
-});
-
-const Navigator = createSwitchNavigator({
-  AuthLoading: AuthLoading,
-  Auth: LoginStackNavigator
+  Home: {
+    screen: AppDrawerNavigator
+  }
 });
 
 export default createAppContainer(Navigator);
