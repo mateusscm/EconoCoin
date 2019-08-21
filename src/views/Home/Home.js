7f7f08e1e5d700296b82f29c00e0d3fe4745ef94
mateusscm <mateus.miranda@hotmail.com.br>
2019-08-21 18:04:06 -0300

    add floating action button

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -3,6 +3,9 @@ import { View, Text, StyleSheet } from "react-native";
 
 import MenuButton from "./../../components/MenuButton/MenuButton";
 
+import Icon from "react-native-vector-icons";
+import { FloatingAction } from "react-native-floating-action";
+
 // const { width: WIDTH } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
@@ -23,11 +26,38 @@ class Home extends Component {
     this.state = {};
   }
   render() {
+    const actions = [
+      {
+        text: "Bob",
+        icon: require("./../../assets/img/android.png"),
+        name: "bt_language",
+        position: 1
+      },
+      {
+        text: "Esponja",
+        icon: require("./../../assets/img/ic_accessibility_white.png"),
+        name: "bt_accessibility",
+        position: 2
+      },
+      {
+        text: "Calça Quadrada",
+        icon: require("./../../assets/img/feedback.png"),
+        name: "bt_room",
+        position: 3
+      }
+    ];
     return (
       <React.Fragment>
         <MenuButton navigation={this.props.navigation} />
         <View style={styles.allCont}>
           <Text style={styles.text}>Home</Text>
+          <FloatingAction
+            color="#3c7d2c"
+            actions={actions}
+            onPressItem={name => {
+              console.log(`selected button: ${name}`);
+            }}
+          />
         </View>
       </React.Fragment>
     );
